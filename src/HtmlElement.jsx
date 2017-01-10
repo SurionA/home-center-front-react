2ab46520b8018a032f417fc390d2127300973b0d
Maxime Frere <mfrere@linagora.com>
2017-01-10 18:01:57 +0100

    First commit

---
+++ src/HtmlElement.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const MainTitle = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #E6E1E3;
+  margin-top: 50px;
+  margin-bottom: 100px;
+`;
+
+export { MainTitle };

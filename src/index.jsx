80fa98dd9dc66806e1b45b28b45eef880aeb4a59
Maxime Frere <mfrere@linagora.com>
2017-09-13 18:29:55 +0200

    Init react app

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import App from './App.jsx';
-import TemperatureChart from './TemperatureChart.jsx';
-import HumidityChart from './HumidityChart.jsx';
-
-render(<App />, document.getElementById('app'));
-render(<TemperatureChart />, document.getElementById('temperature-chart'));
-render(<HumidityChart />, document.getElementById('humidity-chart'));
